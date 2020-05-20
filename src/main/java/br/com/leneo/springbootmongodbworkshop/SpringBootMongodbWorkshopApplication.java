a9b3d8733eb84b61cfa2ff7ae4b9dcf16ddce6e7
Leneo <pauloweskley.dev@gmail.com>
2020-05-20 15:26:08 -0300

    Primeiro Commit.

---
+++ src/main/java/br/com/leneo/springbootmongodbworkshop/SpringBootMongodbWorkshopApplication.java
@@ -0,0 +1,13 @@
+package br.com.leneo.springbootmongodbworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongodbWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/leneo/springbootmongodbworkshop/SpringBootMongodbWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leneo.springbootmongodbworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
